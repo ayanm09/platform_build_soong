116d6cfd0003dd40f87936fd34e06c1bd41c7904
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 11:25:08 -0700

    soong: Add java sources overlay support
    [AgentFabulous - POSP]
    - Adapt to android-r-beta-3, for changes:
      https://android.googlesource.com/platform/build/soong/+/988414c2cf6bfb868df7d402e0bf825d6fd44cc8
      https://android.googlesource.com/platform/build/soong/+/98be1bb00f0fb10e195f26fd2a19952d701a1950
    
    Change-Id: I94143febb0a8afa6a165364d36a40d5120a4e7bc
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- android/config.go
+++ android/config.go
@@ -31,6 +31,8 @@ import (
 	"github.com/google/blueprint/proptools"
 
 	"android/soong/android/soongconfig"
+
+	"octavi/soong/android"
 )
 
 var Bool = proptools.Bool
@@ -605,6 +607,14 @@ func (c *config) DeviceResourceOverlays() []string {
 
 func (c *config) ProductResourceOverlays() []string {
 	return c.productVariables.ProductResourceOverlays
+}
+
+func (c *config) JavaSourceOverlays() string {
+	return String(c.productVariables.Octavi.Java_Source_Overlays)
+}
+
+func (c *config) JavaSourceOverlayModuleWhitelist() []string {
+	return android.OctaviConfig.JavaSourceOverlayModuleWhitelist
 }
 
 func (c *config) PlatformVersionName() string {
